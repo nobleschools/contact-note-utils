2a8b0c2e1098941a96812e83c2338dfe328cbf6a
Joe Ravesloot <jravesloot@noblenetwork.org>
2017-09-21 11:07:24 -0500

    Add headers from persistence reports

--- header_mappings.py
+++ header_mappings.py
@@ -7,19 +7,26 @@ mapped to the related Salesforce field name.
 
 from salesforce_fields import contact_note as cn_fields
 
-
+# input headers will compare header.lower() to the below
 HEADER_MAPPINGS = {
-    'comments'             : cn_fields.COMMENTS,
-    'communication status' : cn_fields.COMMUNICATION_STATUS,
-    'successful?'          : cn_fields.COMMUNICATION_STATUS,
-    'contact'              : cn_fields.CONTACT,
-    'safe_id'              : cn_fields.CONTACT,
-    'date'                 : cn_fields.DATE_OF_CONTACT,
-    'date of contact'      : cn_fields.DATE_OF_CONTACT,
-    'initiated by alum'    : cn_fields.INITIATED_BY_ALUM,
-    'method'               : cn_fields.MODE_OF_COMMUNICATION,
-    'mode of communication': cn_fields.MODE_OF_COMMUNICATION,
-    'subject'              : cn_fields.SUBJECT,
+    "comments"                 : cn_fields.COMMENTS,
+    "communication status"     : cn_fields.COMMUNICATION_STATUS,
+    "successful?"              : cn_fields.COMMUNICATION_STATUS,
+    "contact"                  : cn_fields.CONTACT,
+    "safe_id"                  : cn_fields.CONTACT,
+    "date"                     : cn_fields.DATE_OF_CONTACT,
+    "date of contact"          : cn_fields.DATE_OF_CONTACT,
+    "initiated by alum"        : cn_fields.INITIATED_BY_ALUM,
+    "method"                   : cn_fields.MODE_OF_COMMUNICATION,
+    "mode of communication"    : cn_fields.MODE_OF_COMMUNICATION,
+    "subject"                  : cn_fields.SUBJECT,
+    # from persistence reports
+    "sfid"                     : cn_fields.CONTACT,
+    "cn: subject"              : cn_fields.SUBJECT,
+    "cn: comments"             : cn_fields.COMMENTS,
+    "cn: date of communication": cn_fields.DATE_OF_CONTACT,
+    "cn: communication status" : cn_fields.COMMUNICATION_STATUS,
+    "cn: mode of communication": cn_fields.MODE_OF_COMMUNICATION,
 }
 
 FB_NOTE_HEADERS = (
