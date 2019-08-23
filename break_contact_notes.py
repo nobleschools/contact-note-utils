543004a9118035e85c1a53a526f08272d40ccc26
dmarab2 <derrickbmarable@gmail.com>
2019-08-23 14:25:31 -0500

    #5 Code Refactoring
    Some of the code in break_notes has been factored out into two separate functions, write_rows and get_endpoints_of_duplicate_columns. Docstrings have also been added to all functions.

--- break_contact_notes.py
+++ break_contact_notes.py
@@ -17,59 +17,96 @@ def handle_args():
     return parser.parse_args()
 
 def break_notes(filename):
-    """substitute docstring because this function does way too much right now.
-    """
+    """The main function in this program.
 
+    This function takes a file and puts it through the process described in the
+    documentation at the beginning of the file. In the end a new file is created using the filename
+    of the input file as a base.
+
+    Args:
+        filename: the name of the file to be used in the script.
+    """
     output_filename = 'merged_' + filename
-    column_index = 0
-    duplicate_col_start = 0
-    duplicate_col_end = 0
-    duplicate_col_dict = {}
-    common_col_list = []
-    duplicate_col_list = []
-    header_row_list = []
 
     with open(filename, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
         csv_w_file = open(output_filename, "w", newline='') 
         csv_writer = csv.writer(csv_w_file)
         header_row_list = csv_reader.__next__()
-
-        for row_item in header_row_list:
-            if row_item not in duplicate_col_dict:
-                duplicate_col_dict[row_item] = column_index
-                column_index += 1
-            else:
-                duplicate_col_start = duplicate_col_dict[row_item]
-                duplicate_col_end = column_index - 1
-                break
-        column_index = 0
+        duplicate_col_start, duplicate_col_end = get_endpoints_of_duplicate_columns(header_row_list)
         header_row_list = remove_duplicate_list_items(header_row_list)
-
+        csv_writer.writerow(header_row_list)
         for row in csv_reader:
-            if header_row_list:
-                csv_writer.writerow(header_row_list)
-                header_row_list = []
-            for row_item in row:
-                if column_index < duplicate_col_start:
-                    common_col_list.append(row_item)
-                    column_index += 1
-                elif column_index <= duplicate_col_end:
-                    duplicate_col_list.append(row_item)
-                    column_index += 1
-                else:                    
-                    csv_writer.writerow(common_col_list + duplicate_col_list)
-                    duplicate_col_list = []
-                    duplicate_col_list.append(row_item)
-                    column_index = duplicate_col_start + 1
-            csv_writer.writerow(common_col_list + duplicate_col_list)
-            duplicate_col_list = []    
-            common_col_list = []
-            column_index = 0
+            write_rows(row,csv_writer,duplicate_col_start,duplicate_col_end)            
         csv_w_file.close()
 
 
+def get_endpoints_of_duplicate_columns(header_row_list):
+    """Gets the endpoints of the first set of duplicate columns.
+    
+    The positions first set of duplicate columns in the input file will be used while iterating through
+    the output file, as well as to judge how long each row will be. Therefore it's important they these 
+    endpoints are calculated early.
+
+    Args:
+        header_row_list: A list of strings that are the names of the header rows in the input file.
+
+    Returns:
+        duplicate_col_start and duplicate_col_end, two integers that represent both the start and end
+        of the first set of duplicate columns in the input file.
+    """
+    column_index = 0
+    duplicate_col_start = 0
+    duplicate_col_end = 0
+    duplicate_col_dict = {}
+    for row_item in header_row_list:
+        if row_item not in duplicate_col_dict:
+            duplicate_col_dict[row_item] = column_index
+            column_index += 1
+        else:
+            duplicate_col_start = duplicate_col_dict[row_item]
+            duplicate_col_end = column_index - 1
+            break
+    return duplicate_col_start, duplicate_col_end
+
+
+def write_rows(row, csv_writer, duplicate_col_start, duplicate_col_end):
+    """Write rows to the output file.
+    
+    This function will write a variable amount of rows to an output file for each singular row in the input.
+    The number of rows written is dependent on the amount of duplicate columns in the input. The amount 
+    of columns in each rows is dependent on the duplicate column endpoints that were calculated earlier.
+
+    Args:
+        row: A row of data taken from the input file.
+        csv_writer: The CSV object representing the output file to be written to.
+        duplicate_col_start: The starting point of the duplicate columns in the input file.
+        duplicate_col_end: The ending point of the duplicate columns in the input file.
+    """
+    column_index = 0
+    common_col_list = []
+    duplicate_col_list = []
+    for row_item in row:
+        if column_index < duplicate_col_start:
+            common_col_list.append(row_item)
+            column_index += 1
+        elif column_index <= duplicate_col_end:
+            duplicate_col_list.append(row_item)
+            column_index += 1
+        else:                    
+            csv_writer.writerow(common_col_list + duplicate_col_list)
+            duplicate_col_list = []
+            duplicate_col_list.append(row_item)
+            column_index = duplicate_col_start + 1
+    csv_writer.writerow(common_col_list + duplicate_col_list)
+
+
 def remove_duplicate_list_items(dup_list):
+    """creating a dictionary made of keys from a list and then converting those keys back
+    to a list will remove any duplicate elements from a list while keeping the elements in
+    the same order.
+    """
+
     dup_list = list(dict.fromkeys(dup_list))
     return dup_list
 
